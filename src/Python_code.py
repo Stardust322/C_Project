957619c0aac2342830e24bac3cec774933f9e1f9
이상화 <stardust3292@gmail.com>
2024-12-07 15:23:52 +0900

    File add

--- src/Python_code.py
+++ src/Python_code.py
@@ -1,24 +1,33 @@
-import torch
 import cv2
+import webbrowser
+import torch
 import pathlib
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
+print("Embedding Python...")
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
-model = torch.hub.load('ultralytics/yolov5', 'custom', path=r'C:\Users\stardust\source\repos\Test\best.pt', force_reload=True)
-cap = cv2.VideoCapture(0)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path=r"C:\Users\stardust\source\repos\Test\best.pt", force_reload=True)
+url = 'http://192.168.35.80:81/stream' 
+cap = cv2.VideoCapture(url)
+webbrowser.open(url, new=2)
+if not cap.isOpened():
+    print("스트리밍 연결 안됨.")
 food_class_id = 0
-while cap.isOpened():
+while True:
     ret, frame = cap.read()
     if not ret:
         print("프레임 캡처 실패")
-        break
+        break 
     results = model(frame)
-    for *box, conf, cls in results.xyxy[0]:
-        label = f'{model.names[int(cls)]} {conf:.2f}'
-        cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 2)
-        cv2.putText(frame, label, (int(box[0]), int(box[1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-    cv2.imshow('YOLOv5 Detection', frame)
+    detections = results.xyxy[0]
+    for *box, confidence, class_id in detections.cpu().numpy():
+        x1, y1, x2, y2 = map(int, box)
+        class_id = int(class_id)
+        label = f"{results.names[class_id]} {confidence:.2f}"
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    cv2.imshow("ESP32 Stream - Object Detection", frame)
     detected_classes = results.xywh[0][:, -1].cpu().numpy()
     food_detected = sum(detected_classes == food_class_id)
     if food_detected > 1:
